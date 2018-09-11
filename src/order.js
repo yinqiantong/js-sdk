d8088769e7ffd2f885dee5af032dc0999ee7872a
huzhenjie <huzhenjie.dev@gmail.com>
2018-09-11 11:29:13 +0800

    alipay pc web pay support

--- src/order.js
+++ src/order.js
@@ -5,15 +5,29 @@ function showAlipayH5(formBody) {
     document.forms['alipaysubmit'].submit();
 }
 
+function showDoNotSupport(channel, platform) {
+
+}
+
 export default {
     pay: function (options) {
         const {channel, out_trade_no, platform, pay_body} = options;
         if (channel === 'alipay') {
             switch (platform) {
                 case 'h5':
+                case 'pc':
                     showAlipayH5(pay_body);
-                    break;
+                    return;
+            }
+        } else if (channel === 'wx') {
+            switch (platform) {
+                case 'h5':
+                    return;
+                case 'mp':
+                    return;
             }
         }
+
+        showDoNotSupport(channel, platform);
     }
 }

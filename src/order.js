25dc5731dda2d8af6da3221bd6effe707399efec
huzhenjie <huzhenjie.dev@gmail.com>
2018-09-12 17:07:34 +0800

    support alipay in multiple QR code

--- src/order.js
+++ src/order.js
@@ -1,3 +1,13 @@
+import {
+    getUrlParam,
+    delUrlParam,
+    isWechat,
+    isAlipay,
+    isAndroid,
+    isIos,
+    createWxAuthRedirectUrl
+} from "./utils";
+
 function showAlipayH5(formBody) {
     const element = document.createElement('div');
     element.innerHTML = formBody;
@@ -10,6 +20,38 @@ function showDoNotSupport(channel, platform) {
 }
 
 export default {
+    multipleQrCode: function ({money, wxAppId, showQrCode, createOrder, showH5PaySelector, error}) {
+        if (showQrCode) {
+            showQrCode(delUrlParam(location.href, 'code'))
+        }
+
+        if (isAlipay()) {
+            if (!createOrder) {
+                return error && error('createOrder 尚未配置，暂不支持支付宝')
+            }
+            return createOrder('alipay', 'h5', money, 0)
+        }
+
+        if (isWechat()) {
+            const code = getUrlParam('code');
+            if (!code || code === '') {
+                const currHref = location.href;
+                location.href = createWxAuthRedirectUrl(wxAppId, currHref);
+                return
+            }
+            if (!createOrder) {
+                return error && error('createOrder 尚未配置，暂不支持微信')
+            }
+            return createOrder('wx', 'mp', money, code)
+        }
+
+        if (isAndroid() || isIos()) {
+            if (!showH5PaySelector) {
+                return error && error('showH5PaySelector 尚未配置H5支付')
+            }
+            return showH5PaySelector()
+        }
+    },
     pay: function (options) {
         const {channel, out_trade_no, platform, pay_body} = options;
         if (channel === 'alipay') {
@@ -22,8 +64,10 @@ export default {
         } else if (channel === 'wx') {
             switch (platform) {
                 case 'h5':
+                    alert('微信h5');
                     return;
                 case 'mp':
+                    alert('微信公众号');
                     return;
             }
         }

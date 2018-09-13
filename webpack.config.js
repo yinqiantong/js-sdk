7a28ce9a5a9c6a92cb86fa7d7c9b1dcb6d7b281b
huzhenjie <huzhenjie.dev@gmail.com>
2018-09-13 16:25:36 +0800

    Convert ES6 into ES5 using Babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,17 @@
 module.exports = {
-    entry: "./src/index.js",
+    entry: ["./src/index.js"],
     output: {
         filename: 'yinqiantong.min.js',
         library: "yinqiantong",
         libraryTarget: "umd",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            }
+        ]
     }
 }
